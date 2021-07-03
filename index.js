e91f61f8dea00517471bf4bec81def453caed4f1
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-07-02 18:57:17 -0700

    replace airtable-plus with asyncairtable

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
 require("dotenv").config();
 
-const AirtablePlus = require("airtable-plus");
+const AsyncAirtable = require('asyncairtable');
 const issues = require('./issues')
 
 
-const airtable = new AirtablePlus({
-	baseID: process.env.AIRTABLE_BASE,
-	apiKey: process.env.AIRTABLE_KEY,
-	tableName: "Data Sources",
-});
+const asyncAirtable = new AsyncAirtable(process.env.AIRTABLE_KEY, process.env.AIRTABLE_BASE);
+const TABLE_NAME = "Data Sources"
 
 async function createIssues() {
 	console.log('Starting create issues task...')
-
-	const approvedRecords = await airtable.read({
+	
+	const approvedRecords = await asyncAirtable.select(TABLE_NAME, {
 		filterByFormula: 'Stage = "Approved"'
-	})
+	});
 
 	approvedRecords.forEach(async (r) => {
 		let fields = r.fields
@@ -49,11 +46,13 @@ async function createIssues() {
 		// parallel jobs tend to overwhelm the airtable API throttling,
 		// resulting in issues being created but not noted in airtable.
 		// Alternatively, restructure to diminish airtable calls.
-		let res = await airtable.update(r.id, {
-			fetch_issue_id: fetchNum.toString(),
-			parse_issue_id: parseNum.toString(),
-			normalize_issue_id: normalizeNum.toString()
-		})
+		let res = asyncAirtable.updateRecord(TABLE_NAME, {
+			id: r.id, fields: {
+				fetch_issue_id: fetchNum.toString(),
+				parse_issue_id: parseNum.toString(),
+				normalize_issue_id: normalizeNum.toString()
+			}
+		});
 
 		console.log(`Issues successfully created for ${runnerPath}.`)
 	})
